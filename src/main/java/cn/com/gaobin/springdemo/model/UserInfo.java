02dfd528a1958f7278c18f5149ba361c027d94a9
gaobin <gaobin@workway.com.cn>
2016-10-24 17:23:33 +0800

    用户实体类的更新

--- src/main/java/cn/com/gaobin/springdemo/model/UserInfo.java
+++ src/main/java/cn/com/gaobin/springdemo/model/UserInfo.java
@@ -4,9 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
 /**
- * Created by Administrator on 2016-08-12.
+ * Created by gaobin on 2016-08-12.
  */
 @Entity
 public class UserInfo {
@@ -19,6 +20,25 @@ public class UserInfo {
 
     @Column(length = 20, nullable = false)
     String password;
+
+    @Column(length = 24)
+    String email;
+
+    @Column(length = 24)
+    String phone;
+
+    @Column
+    String weinxin; //微信号
+
+    @Column
+    String LiveCity; //现居城市
+
+    @Column
+    String introduction; //个人简介
+
+    @Column
+    Date registerDate; //注册日期
+
 
     public Integer getId() {
         return id;
@@ -42,5 +62,53 @@ public class UserInfo {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWeinxin() {
+        return weinxin;
+    }
+
+    public void setWeinxin(String weinxin) {
+        this.weinxin = weinxin;
+    }
+
+    public String getLiveCity() {
+        return LiveCity;
+    }
+
+    public void setLiveCity(String liveCity) {
+        LiveCity = liveCity;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
     }
 }
